3e8235c1c0539780ca54a18b6dc06de2c2a1d0de
mock-student <81558782+mock-student@users.noreply.github.com>
2022-09-09 20:47:16 +0800

    Vote for Del

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('Simple DevOps')
+  res.send('Delfrinando is most handsome!!!!!!!')
 })
 
 app.listen(process.env.PORT, ()=>{
